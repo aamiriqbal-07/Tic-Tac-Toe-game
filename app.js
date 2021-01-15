81ecbdfff4eccdbe4bdd5851b071cf498171080e
aamiriqbal-07 <aamir.btech.cs17@iiitranchi.ac.in>
2021-01-15 16:34:50 +0530

    Text change on the top "Let's GO"

--- app.js
+++ app.js
@@ -12,16 +12,16 @@ const drawBoard = () => {
   boxes.forEach((box, index) => {
     let styleString = "";
     if (index < 3) {
-      styleString += `border-bottom: 3px solid var(--purple);`;
+      styleString += `border-bottom: 3px solid var(--seaGreen);`;
     }
     if (index % 3 === 0) {
-      styleString += `border-right: 3px solid var(--purple);`;
+      styleString += `border-right: 3px solid var(--seaGreen);`;
     }
     if (index % 3 === 2) {
-      styleString += `border-left: 3px solid var(--purple);`;
+      styleString += `border-left: 3px solid var(--seaGreen);`;
     }
     if (index > 5) {
-      styleString += `border-top: 3px solid var(--purple);`;
+      styleString += `border-top: 3px solid var(--seaGreen);`;
     }
     box.style = styleString;
 
@@ -93,7 +93,7 @@ restartBtn.addEventListener("click", () => {
   boxes.forEach((box) => {
     box.innerText = "";
   });
-  playText.innerHTML = `Let's Play!!`;
+  playText.innerHTML = `Let's Go!`;
 
   currentPlayer = O_TEXT;
 });
